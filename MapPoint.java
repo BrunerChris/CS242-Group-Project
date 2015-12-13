4c9f71f5b8b9839a61bfd32536d0b966399988a5
BrunerChris <christopher.bruner11@gmail.com>
2015-12-12 21:25:23 -0600

    MapPoint
    I f'ed some things up...this should definitely be up-to-date now.

--- MapPoint.java
+++ MapPoint.java
@@ -3,12 +3,17 @@ public class MapPoint
   private String StartingPoint = null;
   private String EndingPoint = null;
   private int distance = 0;
-
+  private boolean visited = false;
+  private boolean backEdge = false;
+  
   public MapPoint(String sp, String ep, int d)
   {
     StartingPoint = sp;
     EndingPoint = ep;
     distance = d;
+    visited = false;
+    backEdge = false;    
+    
   }
 
   public String getStartPoint()
@@ -25,7 +30,16 @@ public class MapPoint
   {
     return distance;
   }
+  public boolean getVisit()
+  {
+      return visited;
+  }
 
+  public boolean getBackEdge()
+  {
+      return backEdge;
+  }  
+  
   public void setStartPoint(String input)
   {
     StartingPoint = input;
@@ -41,6 +55,16 @@ public class MapPoint
     distance = input;
   }
 
+  public void setVisit(boolean b)
+  {
+      visited = b;
+  }
+
+  public void setBackEdge(boolean t)
+  {
+      backEdge = t;
+  }  
+  
   public String toString()
   {
     String output = "Starting point: " + getStartPoint() + "\nEnding point: " + getEndPoint() + "\nDistance: " + getDistance();
